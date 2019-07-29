97d14c7458a5295ce019bca778392c1d8fc34677
DaiHoang1411 <s3426353@rmit.edu.vn>
2019-07-29 16:42:05 +0700

    Update I2C.c

--- root/source/kernel/I2C.c
+++ root/source/kernel/I2C.c
@@ -1,7 +1,13 @@
 #include "../../include/kernel/I2C.h"
 
-/* C Register (Control Register) - enable interrupts, clear the FIFO, 
+void I2C_init()
+{
+    /* C Register (Control Register) - enable interrupts, clear the FIFO, 
 define a read or write operation and start a transfer */ 
+    I2C_control_t control;
+    bzero (&control, 4);
+    
+
 
 /* the Read field specifies the type of transfer */
 
@@ -19,3 +25,19 @@ and so the bit will always read back as 0. */
 DONE condition. The interrupt remains active until the DONE condition 
 is cleared by writing a 1 to the I2CS.DONE field. Writing a 0 to 
 the INTD field disables interrupts on DONE. */
+
+/* The INTT field enables  interrupts whenever the FIFO is or more 
+empty and needs writing (i.e during a write transfer) - The TXW 
+condition. The interrupt remains active until the TXW condition is 
+cleared by writing sufficient data to the FIO to complete the 
+transfer. Writing a 0 to the INTT field  disables interrupts on TXW. */
+
+/* The INTR field enables interrupts whenever the FIFO is more full 
+and needs reading (i.e during a read transfer) - the RXR condition. 
+The interrupt remains active until the RXW condition is cleared by 
+reading sufficient data from the RX FIFO. Writing a 0 to the INTR 
+field disables interrupts on RXR. */
+
+/*The I2CEN field enables BSC operations. If this bit is 0 then transfer 
+will not be performed. All register accesses are still permitted however. */
+}
