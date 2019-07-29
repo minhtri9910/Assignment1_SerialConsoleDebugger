b82f9eb9c0814312f0fb7ab834869bdd168085ef
DaiHoang1411 <s3426353@rmit.edu.vn>
2019-07-29 12:51:42 +0700

    Update I2C.h

--- root/include/kernel/I2C.h
+++ root/include/kernel/I2C.h
@@ -39,16 +39,15 @@ typedef union I2C_status {
         uint8_t padding : 22; // Reserver
         uint8_t Clock_Stretch_Timeout : 1; /* 1 = Slave has held the SCL signal low (clock stretching) 
         for longer and that specified in the I2CCLKT register Cleared by writing 1 to the field */
-        uint8_t ACK_Error : 1;
-        uint8_t RXF_FIFO_Full : 1;
-        uint8_t TXE_FIFO_Empty : 1;
-        uint8_t RXD_FIFO_contains_Data : 1;
-        uint8_t TXD_FIFO_can_accept_Data : 1;
-        uint8_t RXR_FIFO_needs_Reading_full : 1;
-        uint8_t TXW_FIFO_needs_Writing_full : 1;
-        uint8_t DONE_Transfer_Done : 1;
-        uint8_t TA_Transfer_Active : 1;
-
+        uint8_t ACK_Error : 1; // 0 = No errors detected. 1 = Slave has not acknowledged its address. Cleared by writing 1 to the field
+        uint8_t RXF_FIFO_Full : 1; // 0 = FIFO is not full. 1 = FIFO is full. if a read is underway, no further serial data will be received untill data is read from FIFO.
+        uint8_t TXE_FIFO_Empty : 1; // 0 = FIFO is not empty. 1 = FIFO is empty. If a write is underway, no further serial data can be transmitted untill data is written to the FIFO.
+        uint8_t RXD_FIFO_contains_Data : 1; // 0 = FIFO is empty. 1 = FIFO contains at least 1 byte. CLeared by reading sufficient data from FIFO.
+        uint8_t TXD_FIFO_can_accept_Data : 1; // 0 = FIFO is full. the FIFO cannot accept more data. 1 = FIFO has space for at least 1 byte.
+        uint8_t RXR_FIFO_needs_Reading_full : 1; // 0 = FIFO is less than full and a read is underway. 1 = FIFO is or more full and read is underway. CLeared by reading sufficient data from the FIFO.
+        uint8_t TXW_FIFO_needs_Writing_full : 1; // 0 = FIFO is at least full and a write is underway (or sufficient data to send). 1 = FIFO is less then full and a write is underway. CLeared by writing sufficient data to the FIFO
+        uint8_t DONE_Transfer_Done : 1; // 0 = Transfer not completed. 1 = Transfer complete. Cleared by writing 1.
+        uint8_t TA_Transfer_Active : 1; // 0 = Transfer not active. 1 = Transfer active.
     };
     uint32_t as_int;
 } I2C_status_t;
