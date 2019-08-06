7f02512e87bffce2755cadcc9dcf23580a2fd1cd
Duong Minh Nhat <s3715125@rmit.edu.vn>
2019-08-06 19:19:46 +0700

    Update

--- root/include/kernel/gpio.h
+++ root/include/kernel/gpio.h
@@ -3,7 +3,15 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include "../../include/kernel/uart.h" //to use defined enum for GPIO address 
+#include "uart.h" 
+
+enum
+{
+    GPIO_BASE = 0x3F200000,
+    GPFSEL0 = GPIO_BASE, //GPIO function select
+    GPPUD = (GPIO_BASE + 0x94),
+    GPPUDCLK0 = (GPIO_BASE + 0x98),
+};
 
 void gpio_init();
 
